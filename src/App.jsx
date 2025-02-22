6e5727a862c301db4fd296b1a0238a46469ac819
phonelaptop <jakeyjkim@gmail.com>
2025-02-22 14:22:45 -0800

    Login

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { LoginPage } from './components/LoginPage';
+import { Box } from "@mui/material";
+
+export const App = () => {
+  return (
+    <Box className="App">
+      <LoginPage />
+    </Box>
+  );
+};
