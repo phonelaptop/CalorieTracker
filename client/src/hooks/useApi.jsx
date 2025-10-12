16db6a932116faf298f24d8995c5bb8f63438562
phonelaptop <jakeyjkim@gmail.com>
2025-10-12 10:09:07 -0700

    s

--- client/src/hooks/useApi.jsx
+++ client/src/hooks/useApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: "https://xmttymakxz.us-west-2.awsapprunner.com ",
+  baseURL: "https://xmttymakxz.us-west-2.awsapprunner.com",
   headers: { "Content-Type": "application/json" },
   timeout: 60000,
 });
